52c1433f9791a354070da82d5c7cb2a5e0545e3c
shayalieb <105383935+shayalieb@users.noreply.github.com>
2022-09-13 12:12:17 -0400

    Updated typos...
    Updated the typos and corrected the port number in the log to 8080 which is the correct port number

--- server.js
+++ server.js
@@ -1,22 +1,22 @@
 //creating the server and importing the modules http, fs, and url
-let http = requir('http')
-    fs = requir('fs'),
-    url = requir('url');
+let http = require('http')
+    fs = require('fs'),
+    url = require('url');
 
 
 //assiging to the instance of the HTTP module, which inports the modules and allows you to use it function (creatServer)
 http.createServer((request, response) => {
     let addr = request.url,
     q = url.parse(addr, true)
-    fielPath = '';
+    filePath = '';
 
     if (q.pathname.includes('documentation')) {
-        fielPath = (__dirname + '/documentation.html');
+        filePath = (__dirname + '/documentation.html');
     } else {
-        fielPath = 'index.html';
+        filePath = 'index.html';
     }
 
-    fs.readFile(fielPath, (err, data) => {
+    fs.readFile(filePath, (err, data) => {
         if(err) {
             throw err;
         }
@@ -27,5 +27,5 @@ http.createServer((request, response) => {
     });
 
 }).listen(8080);
-console.log('The test server is running on port 880');
+console.log('The test server is running on port 8080');
 
